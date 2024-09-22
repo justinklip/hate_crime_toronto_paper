ba4a233b0f662570e2ecfbc55d66afdead8378c6
justinklip <123585524+justinklip@users.noreply.github.com>
2024-09-22 19:04:17 -0400

    Update 02-data_cleaning.R
    Creates the data cleaning file which cleans up the dataset

--- hate_crime_paper/scripts/02-data_cleaning.R
+++ hate_crime_paper/scripts/02-data_cleaning.R
@@ -1,44 +1,47 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw hate crime data downloaded from Open Data Toronto
+# Author: Justin Klip 
+# Date: 22 September 2024
+# Contact: justin.klip@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 01-download_data.R
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
-
+library(janitor)
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+hate_crime_raw_data <- read_csv("data/raw_data/hate_crime_raw_data.csv")
+
+hate_crime_raw_data
+head(hate_crime_raw_data[7:12])
+
+
+#Most of our variable names are pretty clear, except hood and neighborhood
+#For these, delete the old version which is 140 version, we already have updated 158 version
+#Also not really interested in time of event for this paper, or reported date, remove those too.
+cleaned_hate_crime_data <- hate_crime_raw_data[,!names(hate_crime_raw_data) %in% c("HOOD_140", "NEIGHBOURHOOD_140", "REPORTED_TIME", "REPORTED_YEAR", "REPORTED_DATE", "REPORTED_TIME", "OCCURENCE_TIME")]
+
+#Make the column names easier to type
+cleaned_hate_crime_data <- clean_names(cleaned_hate_crime_data)
+
+#Examine the data
+cleaned_hate_crime_data
+
+#Confirm if there are any missing values
+is.na(cleaned_hate_crime_data)
+
+#There appears to be one, sum to find out how many there are
+sum(is.na(cleaned_hate_crime_data))
+
+#Look at the rows in question
+df_with_na <- cleaned_hate_crime_data[rowSums(is.na(cleaned_hate_crime_data)) > 0, ]
+print(n = 37, df_with_na)
+
+#They all are specifically for location type, so we'll just rename to unspecified
+cleaned_hate_crime_data[is.na(cleaned_hate_crime_data)] <- "unspecified"
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_hate_crime_data, "data/analysis_data/hate_crime_analysis_data.csv")
