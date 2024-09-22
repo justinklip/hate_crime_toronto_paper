641dd6b20d8f37a4582c697855c57a6623a666ea
justinklip <123585524+justinklip@users.noreply.github.com>
2024-09-22 19:04:59 -0400

    Update 03-test_data.R
    Updates the test data to include 4 different tests on our cleaned data

--- hate_crime_paper/scripts/03-test_data.R
+++ hate_crime_paper/scripts/03-test_data.R
@@ -1,15 +1,37 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests the Data for to check if everything is order
+# Author: Justin Klip
+# Date: 22 September 2024 [...UPDATE THIS...]
+# Contact: justin.klip@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 02-data_cleaning.R
+# Any other information needed? None
 
 
-#### Workspace setup ####
+### Workspace setup ###
 library(tidyverse)
-# [...UPDATE THIS...]
 
+### Data Acquisition ###
+hate_crime_analysis_data <- read_csv("data/analysis_data/hate_crime_analysis_data.csv")
 #### Test data ####
+
+# One: NA Test
+sum(is.na(hate_crime_analysis_data))
+
+# Two: Duplicates Test
+sum(duplicated(hate_crime_analysis_data))
+
+# Three : Empty String Test
+sum(hate_crime_analysis_data$location_type == "")
+sum(hate_crime_analysis_data$race_bias == "")
+sum(hate_crime_analysis_data$ethnicity_bias == "")
+sum(hate_crime_analysis_data$religion_bias == "")
+sum(hate_crime_analysis_data$sexual_orientation_bias == "")
+sum(hate_crime_analysis_data$primary_offence == "")
+sum(hate_crime_analysis_data$gender_bias == "")
+sum(hate_crime_analysis_data$division == "")
+
+# Four: Look at data set for any glaring issues
+summary(hate_crime_analysis_data)
+
+#Since most of our data is string data, this isn't a surprising result.
